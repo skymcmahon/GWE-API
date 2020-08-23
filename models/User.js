ae7ca4fd38bb010b3880227c88196dde19dfbb2a
Sky McMahon <sky.m.mcmahon@gmail.com>
2020-08-23 15:47:08 -0600

    User model / auth middleware file structure

--- models/User.js
+++ models/User.js
@@ -3,9 +3,21 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
 	id: Schema.Types.ObjectId,
-	email: String,
-	username: String,
-	pasword: String,
+	email: {
+		type: String,
+		unique: true,
+		required: [true, 'Email is required'],
+	},
+	password: {
+		type: String,
+		minlength: 4,
+		required: [true, 'Password is required'],
+	},
+	username: {
+		type: String,
+		unique: true,
+		required: [true, 'Userame is required'],
+	},
 });
 
 const User = mongoose.model('User', userSchema);
